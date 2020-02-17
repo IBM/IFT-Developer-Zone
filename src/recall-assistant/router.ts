970eccb4e0349f705cf6bd3a145960bb75ac68cf
NATHAN R. BARRY <nrbarry@us.ibm.com>
2020-02-17 09:24:38 -0500

    Remove incorrect copyright notices

--- src/recall-assistant/router.ts
+++ src/recall-assistant/router.ts
@@ -1,15 +1,3 @@
-/*
- * IBM Confidential
- * OCO Source Materials
- * 5900-A1Y
- *
- * © Copyright IBM Corp. 2019
- *
- * The source code for this program is not published or
- * otherwise divested of its trade secrets, irrespective of
- * what has been deposited with the U.S. Copyright Office.
- */
-
 import { Router } from 'express';
 import { getEpcsHandler,
          getEpcsWithTransformsHandler,
