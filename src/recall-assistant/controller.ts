b27c3d4e371beed4b62e141eaaa54c2b0a1f7f6d
p-marinha <pmarinha@in.ibm.com>
2020-05-11 21:54:16 +0530

    feat: Adding new retailer specific scenario

--- src/recall-assistant/controller.ts
+++ src/recall-assistant/controller.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as _ from 'lodash';
 
 import { getEpcs, getTransformOutputEpcs, getTransactions } from './ift-service';
+import { getSourceEPCData } from './retailer-actions';
 
 // Catch errors that occur in asynchronous code and pass them to Express for processing
 export const catchAsync = fn => (...args) => fn(...args).catch(args[2]); // args[2] is next
@@ -38,4 +39,14 @@ export const getTransactionsHandler: express.RequestHandler = catchAsync(async (
   // From the list of bad EPCs (harvested or produced), find aggregations that reference transactions
   // (purchase orders and despatch advice documents)
   return res.status(200).json(await getTransactions(req, totalEpcs));
+});
+
+// controller to get the commissioned (most upstream) epc's
+export const getCommissionedEpcsHandler: express.RequestHandler = catchAsync(async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  // This will get all the commisoned input epcs and related data
+  return res.status(200).json(await getSourceEPCData(req));
 });
--- src/recall-assistant/ift-service.ts
+++ src/recall-assistant/ift-service.ts
@@ -2,7 +2,26 @@ import { config } from '../app';
 import * as rp from 'request-promise-native';
 import * as _ from 'lodash';
 
-const EVENT_PAGE_SIZE = 30;
+export const constants = {
+  // Depth of trace
+  DEPTH: 30,
+  // Pagination limit to be used for trace apis
+  PAGE_SIZE: 30,
+  // URN for GS1 standard SGLN
+  URN_GS1_SGLN: 'urn:epc:id:sgln:',
+  // URN for GS1 standard SGTIN
+  URN_GS1_SGTIN: 'urn:epc:id:sgtin:',
+  // URN for IFT standard SGTIN
+  URN_IFT_SGTIN: 'urn:ibm:ift:product:serial:obj:',
+  // URN for GS1 SGTIN without no lots
+  URN_PAT_SGTIN: 'urn:epc:idpat:sgtin:',
+  // URN for GS1 standard LGTIN
+  URN_GS1_LGTIN: 'urn:epc:class:lgtin:',
+  // URN for IFT standard LGTIN
+  URN_IFT_LGTIN: 'urn:ibm:ift:product:lot:class:',
+  // URN for IFT GTIN
+  URN_IFT_GTIN: 'urn:ibm:ift:product:class:',
+};
 
 // Helper method to take constraints and build parameters for the trace URL
 export function getTraceRestraintParameters(location_id: string,
@@ -84,8 +103,7 @@ export async function getTransformOutputEpcs(req, inputEpcs: string[]) {
 
     // loop through the epcs 30 or PAGE_SIZE at a time and make the event api calls
     while (epcIds.length > 0) {
-      pagedEPC = epcIds.splice(0, EVENT_PAGE_SIZE);
-
+      pagedEPC = epcIds.splice(0, constants.PAGE_SIZE);
       // form the URL and make the calls
       const eventCallUriParamWithEPC = `${pagedEPC && pagedEPC.length > 0 ? `&epc_id[]=${pagedEPC.join('&epc_id[]=')}` : ''}`;
       const eventsCallUri = `${config.ift_url}/events?event_type[]=transformation${eventCallUriParamWithEPC}`;
@@ -128,7 +146,7 @@ export async function getTransactions(req, inputEpcs: string[]) {
 
     // loop through the epcs 30 or PAGE_SIZE at a time
     while (epcIds.length > 0) {
-      pagedEPC = epcIds.splice(0, EVENT_PAGE_SIZE);
+      pagedEPC = epcIds.splice(0, constants.PAGE_SIZE);
 
       // form the URL and make the calls
       const eventCallUriParamWithEPC = `${pagedEPC && pagedEPC.length > 0 ? `&epc_id[]=${pagedEPC.join('&epc_id[]=')}` : ''}`;
@@ -173,4 +191,420 @@ export async function processPromiseList(promiseList: string[], eventType: strin
     throw err;
   });
 
+}
+
+// Find all EPCs (lots and serials) commissioned for particular product
+export async function getProductLotsAndSerials(req) {
+  const restraintParameters = getTraceRestraintParameters('',
+                                                          req.query.product_id,
+                                                          req.query.event_start_timestamp,
+                                                          req.query.event_end_timestamp);
+  const lotsAndSerialsCallUri = `${config.ift_url}/lots_and_serials?limit=500&${restraintParameters}`;
+  console.info(`Trace call to get product lots and serial EPCs: ${lotsAndSerialsCallUri}`);
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: req.headers['authorization'],
+    },
+    uri: lotsAndSerialsCallUri,
+    method: 'GET',
+  };
+  // Issue request to the trace API
+  return rp(options).then((response: any) => {
+    const epcRespObj = JSON.parse(response);
+    // Get a list of all unique EPCs referenced in the matching events
+    const epcs: string[] = epcRespObj.lots_and_serials.reduce((epcArry, currentValue) => {
+      return [...epcArry, currentValue.id];
+    }, []);
+    return epcs;
+  }).catch((err) => {
+    console.error(`Error getting EPCs from commission events: ${err}`);
+    throw err;
+  });
+}
+
+// Get all the aggregation/observation events for given lots and serials
+// TODO: make it more generic to support other types
+export async function getEvents(req, inputAssetIds: string[], bizStep?: string[]) {
+  let pagedAssets: string[] = [];
+  const eventsData: string[] = [];
+  const eventsPromiseList = [];
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: req.headers['authorization'],
+    },
+    uri: '',
+    method: 'GET',
+  };
+
+  if (inputAssetIds && Array.isArray(inputAssetIds) && inputAssetIds.length > 0) {
+    // loop through the assetids 30 or PAGE_SIZE at a time
+    while (inputAssetIds.length > 0) {
+      pagedAssets = inputAssetIds.splice(0, constants.PAGE_SIZE);
+      // form the URL and make the calls
+      const eventCallUriParamWithAssets = `${pagedAssets && pagedAssets.length > 0
+        ? `&asset_id[]=${pagedAssets.join('&asset_id[]=')}` : ''}`;
+      // optional biz_step
+      const eventBizStep = `${bizStep && bizStep.length > 0
+        ? `&biz_step[]=${bizStep.join('&biz_step[]=')}` : ''}`;
+      // filter by event_end_timestamp so that you dont get events past the date searched for
+      // const eventEndTimeParams = getTraceRestraintParameters('', [], '', req.query.event_end_timestamp);
+      const eventEndTimeParams = '';
+      const eventCallUri = `${config.ift_url}/events?event_type[]=aggregation&event_type[]=observation${
+        eventCallUriParamWithAssets}${eventBizStep}${eventEndTimeParams}`;
+      console.info(`Trace call to get all events from asset ids: ${eventCallUri}`);
+
+      options.uri = eventCallUri;
+      // Issue request to the trace API and save in an array of promises
+      eventsPromiseList.push(rp(options));
+    }
+  }
+
+  return Promise.all(eventsPromiseList).then((eventResponse: any) => {
+    eventResponse.forEach((response) => {
+      const eventsObj = JSON.parse(response);
+      eventsData.push(...eventsObj.events);
+    });
+    return eventsData;
+  }).catch((err) => {
+    console.error(`Error getting events from asset IDs: ${err}`);
+    throw err;
+  });
+}
+
+// Run a trace on all EPC's and return the asset id's
+export async function runTrace(req, inputEPCs: string[], upstream = false, downstream = false) {
+  const tracePromiseList = [];
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: req.headers['authorization'],
+    },
+    uri: '',
+    method: 'GET',
+  };
+  if (inputEPCs && inputEPCs.length > 0) {
+    inputEPCs.forEach(epcId => {
+      // foreach EPC trace upstream
+      const traceUri = `${config.ift_url}/epcs/${epcId}/trace?depth=${constants.DEPTH}${
+        upstream ? `&upstream=true` : ''}${
+        downstream ? `&downstream=true` : ''}`;
+      console.info(`Trace call to get the EPC/trace: ${traceUri}`);
+
+      options.uri = traceUri;
+      // Issue request to the trace API and save in an array of promises
+      tracePromiseList.push(rp(options));
+    });
+
+    return Promise.all(tracePromiseList).then((traceResponse: any) => {
+      const traceResults = [];
+      traceResponse.forEach((response) => {
+        const traceObj = JSON.parse(response);
+        traceResults.push(traceObj.trace);
+      });
+      return traceResults;
+    }).catch((err) => {
+      console.error(`Error tracing on epcs: ${err}`);
+      throw err;
+    });
+  }
+}
+
+/**
+ * Method to call location API to fetch location data
+ */
+export async function getLocationsData(req, locationIds) {
+  let locations: string[] = [];
+  const promiseList = [];
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: req.headers['authorization'],
+    },
+    uri: '',
+    method: 'GET',
+  };
+
+  if (locationIds && locationIds.length > 0) {
+    // loop through the locationIds 30 or PAGE_SIZE at a time
+    while (locationIds.length > 0) {
+      locations = locationIds.splice(0, constants.PAGE_SIZE);
+      // form the URL and make the calls
+      const locationsUri = `${config.ift_url}/locations?${locations && locations.length > 0
+        ? `location_id[]=${locations.join('&location_id[]=')}` : ''}`;
+      console.info(`Trace call to get location data: ${locationsUri}`);
+
+      options.uri = locationsUri;
+      // Issue request to the trace API and save in an array of promises
+      promiseList.push(rp(options));
+    }
+  }
+
+  return Promise.all(promiseList).then((response: any) => {
+    const locationData = [];
+    response.forEach((data) => {
+      const locationsObj = JSON.parse(data);
+      locationData.push(...locationsObj.locations);
+    });
+    return locationData;
+  }).catch((err) => {
+    console.error(`Error getting locations: ${err}`);
+    throw err;
+  });
+}
+
+/**
+ * Method to call the Products API and fetch product information
+ */
+export async function getProductsData(req, producIds) {
+  let products: string[] = [];
+  const promiseList = [];
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: req.headers['authorization'],
+    },
+    uri: '',
+    method: 'GET',
+  };
+
+  if (producIds && Array.isArray(producIds) && producIds.length > 0) {
+    // loop through the productIds 30 or PAGE_SIZE at a time
+    while (producIds.length > 0) {
+      products = producIds.splice(0, constants.PAGE_SIZE);
+      // form the URL and make the calls
+      const productUri = `${config.ift_url}/products?${products && products.length > 0
+        ? `product_id[]=${products.join('&product_id=')}` : ''}`;
+      console.info(`Trace call to get product data: ${productUri}`);
+
+      // Used for epcs with special chars in it like '+', but it doesnt work
+      // options.uri = encodeURIComponent(productUri);
+      options.uri = productUri;
+      // Issue request to the trace API and save in an array of promises
+      promiseList.push(rp(options));
+    }
+  }
+
+  return Promise.all(promiseList).then((response: any) => {
+    const productData = [];
+    response.forEach((data) => {
+      const productsObj = JSON.parse(data);
+      productData.push(...productsObj.products);
+    });
+    return productData;
+  }).catch((err) => {
+    console.error(`Error getting product info: ${err}`);
+    throw err;
+  });
+}
+
+// Method to get all the epcEvent mapping from the traced response
+export function getEpcEventsMapFromTrace(traceResponse): {} {
+  const epcEventMap = { outputs: {} , inputs: [{}] };
+  epcEventMap.outputs = {
+    epc_id : traceResponse.epc_id,
+    // events: traceResponse.events
+    events: traceResponse.events.filter((event) => {
+      return (event.asset_id.includes('observation') || event.asset_id.includes('aggregation'));
+    })
+  };
+  epcEventMap.inputs = this.getUpstreamEventsAndEPCs(traceResponse.input_epcs);
+  return epcEventMap;
+}
+
+// Recursively loop through the EPC tree to get all events
+export function getUpstreamEventsAndEPCs(epcs) {
+  if (epcs === undefined || !epcs.length) {
+    return [];
+  }
+
+  return epcs.reduce((allEvents, epc) => { // foreach in the list do the following
+    if (epc.input_epcs.length > 0) {
+      // if there exist input epcs, traverse further in the tree
+      allEvents.push(...this.getUpstreamEventsAndEPCs(epc.input_epcs));
+    } else {
+      // if there are no more inputs, return the edge events
+      allEvents.push({
+        epc_id : epc.epc_id,
+        // events: epc.events
+        events: epc.events.filter((event) => {
+          return (event.asset_id.includes('observation') || event.asset_id.includes('aggregation'));
+        })
+      });
+    }
+    return allEvents;
+  }, []);
+}
+
+// Method to call purchase order transactions API to fetch transaction data
+export async function getTransactionsData(req, trasactionIds, type) {
+  if (!trasactionIds || !type) {
+    return; // return if no type or transaction list is passed.
+  }
+
+  let transactions: string[] = [];
+  const promiseList = [];
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: req.headers['authorization'],
+    },
+    uri: '',
+    method: 'GET',
+  };
+
+  if (trasactionIds && Array.isArray(trasactionIds) && trasactionIds.length > 0) {
+    // loop through the locationIds 30 or PAGE_SIZE at a time
+    while (trasactionIds.length > 0) {
+      transactions = trasactionIds.splice(0, constants.PAGE_SIZE);
+      // form the URL and make the calls
+      const transactionsUri = `${config.ift_url}/transactions/${
+      type && type === 'PO' ? 'purchase_orders' : type === 'DA' ? 'despatch_advices' : type === 'RA' ? 'receive_advices' : '' }?${
+      transactions && transactions.length > 0 ? `transaction_id[]=${transactions.join('&transaction_id[]=')}` : ''}`;
+      console.info(`Trace call to get transaction PO data: ${transactionsUri}`);
+
+      options.uri = transactionsUri;
+      // Issue request to the trace API and save in an array of promises
+      promiseList.push(rp(options));
+    }
+  }
+
+  return Promise.all(promiseList).then((response: any) => {
+    const transactionData = [];
+    response.forEach((data) => {
+      const dataObj = JSON.parse(data);
+      if (type === 'po') {
+        transactionData.push(...dataObj.purchase_orders);
+      } else if (type === 'po') {
+        transactionData.push(...dataObj.despatch_advices);
+      } else if (type === 'po') {
+        transactionData.push(...dataObj.receive_advices);
+      }
+    });
+    return transactionData;
+  }).catch((err) => {
+    console.error(`Error getting transaction data: ${err}`);
+    return []; // return for now (since there are cases for invalid transaction ids)
+  });
+}
+
+// get LGTIN using epcClass
+export function getLGTIN(epc: any): { gtin: string, lotOrSerialNo: string, valid: boolean } {
+  let epcClass;
+  let subProductNo1;
+  let subProductNo2;
+  let partialGTIN;
+  let gtin;
+  let lot;
+
+  // handle IBM-issued (non-GS1)
+  if (epc && epc.indexOf(constants.URN_IFT_LGTIN) >= 0) {  // IFT ID
+    epcClass = explode(epc, constants.URN_IFT_LGTIN);
+    subProductNo1 = explode(epcClass, '.', 0);
+    subProductNo2 = explode(epcClass, '.', 1);
+    gtin = `${constants.URN_IFT_GTIN}${subProductNo1}.${subProductNo2}`;
+    lot = explode(epcClass, '.', 2);
+  } else if (epc && epc.indexOf(constants.URN_GS1_LGTIN) >= 0) {  // GS1 ID
+    epcClass = explode(epc, constants.URN_GS1_LGTIN);
+    subProductNo1 = explode(epcClass, '.', 0);
+    subProductNo2 = explode(epcClass, '.', 1);
+    partialGTIN =
+      `${subProductNo2.substring(0, 1)}${subProductNo1}${subProductNo2.substring(1, subProductNo2.length)}`;
+    const lastDigit = calcCheckDigit(partialGTIN);
+    gtin = `${partialGTIN}${lastDigit}`;
+    lot = explode(epcClass, '.', 2);
+  } else {
+    return {
+      gtin: '',
+      lotOrSerialNo: '',
+      valid: false,
+    };
+  }
+
+  return {
+    gtin,
+    lotOrSerialNo: lot,
+    valid: true,
+  };
+}
+
+// gets serials and gtin from SGTIN using epcList
+// @param epc array of SGTINs in epcList
+export function getSGTIN(epc: any): { gtin: string, lotOrSerialNo: string, valid: boolean } {
+  let epcClass;
+  let subProductNo1;
+  let subProductNo2;
+  let partialGTIN;
+  let gtin;
+  let serialNo;
+
+  if (epc && epc.indexOf(constants.URN_GS1_SGTIN) >= 0) {  // GS1 ID
+    epcClass = explode(epc, constants.URN_GS1_SGTIN);
+    subProductNo1 = explode(epcClass, '.', 0);
+    subProductNo2 = explode(epcClass, '.', 1);
+    partialGTIN =
+      `${subProductNo2.substring(0, 1)}${subProductNo1}${subProductNo2.substring(1, subProductNo2.length)}`;
+    const lastDigit = calcCheckDigit(partialGTIN);
+    gtin = `${partialGTIN}${lastDigit}`;
+    serialNo = explode(epcClass, '.', 2);
+  } else if (epc && epc.indexOf(constants.URN_IFT_SGTIN) >= 0) {  // IFT ID
+    epcClass = explode(epc, constants.URN_IFT_SGTIN);
+    subProductNo1 = explode(epcClass, '.', 0);
+    subProductNo2 = explode(epcClass, '.', 1);
+    gtin = `${constants.URN_IFT_GTIN}${subProductNo1}.${subProductNo2}`;
+    serialNo = explode(epcClass, '.', 2);
+  } else if (epc && epc.indexOf(constants.URN_PAT_SGTIN) >= 0) { // SGTIN w/o serial
+    epcClass = explode(epc, constants.URN_PAT_SGTIN);
+    subProductNo1 = explode(epcClass, '.', 0);
+    subProductNo2 = explode(epcClass, '.', 1);
+    partialGTIN =
+      `${subProductNo2.substring(0, 1)}${subProductNo1}${subProductNo2.substring(1, subProductNo2.length)}`;
+    const lastDigit = calcCheckDigit(partialGTIN);
+    gtin = `${partialGTIN}${lastDigit}`;
+    serialNo = '';
+  } else {
+    return {
+      gtin: '',
+      lotOrSerialNo: '',
+      valid: false,
+    };
+  }
+
+  return {
+    gtin,
+    lotOrSerialNo: serialNo,
+    valid: true,
+  };
+}
+
+// custom string explode function
+export function explode(input: string, separator: string, index?: number): string {
+  if (input !== 'undefined') {
+    if (typeof index !== 'undefined' && index !== null) {
+      return (input.split(separator)[index] !== undefined)
+        ? input.split(separator)[index].replace(/\s+/, '')
+        : input;
+    }
+    return input.split(separator).join(' ');
+  }
+  return '';
+}
+
+// check digit formula
+function calcCheckDigit(s: string): number {
+  let result = 0;
+  const rs = s.split('').reverse().join('');
+
+  for (let counter = 0; counter < rs.length; counter += 1) {
+    result = result + parseInt(rs.charAt(counter), 10) * Math.pow(3, ((counter + 1) % 2));
+  }
+  return (10 - (result % 10)) % 10;
 }
